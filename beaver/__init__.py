e28fb18e500feb54cc7812721289503fdaac0754
Alejandro Piad <apiad@apiad.net>
2025-11-09 17:50:19 +0100

    Bump version to 0.25.1

--- beaver/__init__.py
+++ beaver/__init__.py
@@ -2,7 +2,7 @@ from .core import BeaverDB
 from .types import Model
 from .collections import Document, WalkDirection
 
-__version__ = "0.24.7"
+__version__ = "0.25.1"
 
 __all__ = [
     "BeaverDB",
